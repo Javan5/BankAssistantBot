c6093bf1aaacd8a7726122a2f1b3a46602d9e3bc
BushuevIvan <vtf9755@gmail.com>
2022-07-19 21:19:28 +0300

    TB-18: Создание и загрузка телеграмм бота BankAssistantBot

---
+++ src/main/java/com/example/BankAssistantBot/BankAssistantBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankAssistantBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAssistantBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAssistantBotApplication.class, args);
+	}
+
+}
